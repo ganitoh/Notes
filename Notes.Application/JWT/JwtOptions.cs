7f67bcc61b1cd7b60242a54b587a50255fbbcf8a
Vadim <vadim.gan2018@yandex.ru>
2024-05-05 13:23:06 +0300

    add jwt for applicaton

---
+++ Notes.Application/JWT/JwtOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Notes.Application.JWT
+{
+    public class JwtOptions
+    {
+        public string SecretKey { get; set; } = string.Empty;
+        public string Issuer { get; set; } = string.Empty;
+        public string Audiance { get; set; } = string.Empty;
+        public int ExpireHource { get; set; }
+    }
+}
---
+++ Notes.Application/JWT/JwtProvider.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Notes.Domain.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+
+namespace Notes.Application.JWT
+{
+    public class JwtProvider
+    {
+        private readonly JwtOptions _options;
+
+        public JwtProvider(IOptions<JwtOptions> options)
+        {
+            _options = options.Value;
+        }
+
+        public string GenereateToken(User user)
+        {
+            Claim[] claims = [new("id", user.Id.ToString())];
+
+            var signingCredentails = new SigningCredentials(
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.SecretKey)),
+                SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                signingCredentials:  signingCredentails,
+                expires: DateTime.UtcNow.AddHours(_options.ExpireHource));
+
+            var jwtTokenValue = new  JwtSecurityTokenHandler().WriteToken(token);
+            return jwtTokenValue;
+        }
+    }
+}
